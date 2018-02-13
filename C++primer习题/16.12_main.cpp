40639cf59e0ab51bdf0c6210bf9e11737c41e851
Island-c <liu0148@outlook.com>
2018-02-14 01:03:34 +0800

    2.14

---
+++ C++primer习题/16.12_main.cpp
@@ -0,0 +1,20 @@
+#include "my_head.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <memory>
+#include<string>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	Blob<int> squares_1 = { 0,1,2,3,4,5,6,7,8,9 };
+	Blob<int> squares_2 = { 0,1,2,3,4,5,6,7,8,9 };
+	BlobPtr<int> ptr(squares_1,3);
+	if(squares_1==squares_2)
+		cout << ptr.deref() << endl;
+	cout << squares_2[3] << endl;
+	system("pause");
+	return 0;
+
+}
